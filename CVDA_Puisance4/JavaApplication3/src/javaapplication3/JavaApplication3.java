3a7ee86f41e95783294f74c73c7359560b327623
Youmustkidding <32686212+Youmustkidding@users.noreply.github.com>
2017-12-12 15:56:37 +0100

    colonne fix

--- CVDA_Puisance4/JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ CVDA_Puisance4/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -44,34 +44,39 @@ public class JavaApplication3 {
                 
             }System.out.println("");
         }
+        
+        if (joueur == true){
+            System.out.print("\nJoueur 1 : ");
+        } else {
+            System.out.print("\nJoueur 2 : ");
+        }
     }
     
     public static void jouer(){
         int jeton;
         if (joueur == true){
             jeton=1;
-            System.out.println("\nJoueur 1");
         } else {
             jeton =2;
-            System.out.println("\nJoueur 2");
         }
         
-        int collone = -1;
-        while (collone < 0 || collone > 6 ){
-            System.out.println("Quel collone ?");
+        int colonne = -1;
+        while (colonne < 0 || colonne > 6 ){
+            System.out.print("Quel colonne ? ");
             Scanner input = new Scanner(System.in);
-            collone = input.nextInt();
-            collone --;
+            colonne = input.nextInt();
+            colonne --;
         }
         
         
         
         int i,j;
+        outerloop:
         for(i=0;i<grille.length;i++)
         {
             for(j=0;j<grille[i].length;j++)
             {
-                if(j==collone){                         //Trouve la collone
+                if(j==colonne){                         //Trouve la colonne
                     if(grille[i][j]== 0){               //Vérifie que la première case est vide
                         if((i+1)>grille.length-1) {       //Vérifie que la ligne suivante n'est pas dans le tableau
                             grille[i][j] = jeton;       //Met un jeton
@@ -80,12 +85,12 @@ public class JavaApplication3 {
                                grille[i][j] = jeton;
                             }
                         }
-                    } else {
-                    System.out.println("Collone pleine");
-                    break;
+                    } else if (i==0){                               //Cas où la colonne est pleine
+                        System.out.println("\nColonne pleine. Rejouez");
+                        joueur = !joueur;
+                        break outerloop;
                     }
-                } 
-                
+                }    
             }
         }
         
